7e7318730740aaef35317df8fdd20358dfe087df
MikeJMcGuire <mike@mcguire.mx>
2021-02-17 14:16:00 +1100

    Updates

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-ADMIN_PW=$(jq -r .AdminPassword /data/options.json | htpasswd -nb -B admin -i | cut -d ":" -f 2)
+ADMIN_PW=$(jq -r .AdminPassword /data/options.json | htpasswd -nbiB admin | cut -d ":" -f 2)
 
 /opt/portainer/portainer --admin-password=\'$ADMIN_PW\' --host=unix:///run/docker.sock
