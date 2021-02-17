c2cb1b4afbffe048d7338910335e41f935c153ad
MikeJMcGuire <mike@mcguire.mx>
2021-02-17 15:02:33 +1100

    Updates

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #/bin/sh
 
-ADMIN_PW=$(jq -r .AdminPassword /data/options.json)
-ADMIN_HASH=$(htpasswd -nbB admin $ADMIN_PW | cut -d ":" -f 2)
+echo portainer > /tmp/initial
 
-/opt/portainer/portainer --admin-password=\'$ADMIN_HASH\' --host=unix:///run/docker.sock
+/opt/portainer/portainer --admin-password-file=/tmp/initial --host=unix:///run/docker.sock
